564422473c55a234675cd70f18ebb17672f63603
Maxsonin <maximles.lv@gmail.com>
2025-09-16 18:42:57 +0300

    fix: remove vite-plugin-virtual

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,46 @@
-import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
-import { defineConfig, type PluginOption } from "vite";
-import checker from "vite-plugin-checker";
-import viteCompression from "vite-plugin-compression";
-import Inspect from "vite-plugin-inspect";
-import svgr from "vite-plugin-svgr";
-import virtual from "vite-plugin-virtual";
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig, type PluginOption } from 'vite';
+import checker from 'vite-plugin-checker';
+import viteCompression from 'vite-plugin-compression';
+import Inspect from 'vite-plugin-inspect';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		svgr(),
-		checker({
-			typescript: true,
-			biome: {
-				command: "check",
-			},
-		}),
-		viteCompression({
-			algorithm: "brotliCompress",
-			ext: ".br",
-		}),
-		Inspect(),
-		virtual({
-			"virtual:module": `export default { hello: 'world' }`,
-			"virtual:config": { hello: "world" },
-		}),
-	],
-	build: {
-		minify: "terser",
-		terserOptions: {
-			compress: {
-				drop_console: true,
-				drop_debugger: true,
-			},
-			format: {
-				comments: false,
-			},
-		},
-		rollupOptions: {
-			plugins: [
-				visualizer({
-					filename: "dist/stats.html",
-					open: true,
-				}) as PluginOption,
-			],
-		},
-	},
+  plugins: [
+    react(),
+    svgr(),
+    checker({
+      typescript: true,
+      biome: {
+        command: 'check',
+      },
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+    Inspect(),
+  ],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: false,
+      },
+    },
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          filename: 'dist/stats.html',
+          open: true,
+        }) as PluginOption,
+      ],
+    },
+  },
 });
