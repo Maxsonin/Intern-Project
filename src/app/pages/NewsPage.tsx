088a12116533dca32cd5985fdffa89956ee3ade7
Maxsonin <maximles.lv@gmail.com>
2025-09-17 14:25:28 +0300

    feature: add news to NewsPage

--- src/app/pages/NewsPage.tsx
+++ src/app/pages/NewsPage.tsx
@@ -1,4 +1,30 @@
+import NewsCard from "@/features/news/components/NewsCard";
+
+const newsData = [
+	{
+		id: 1,
+		title: "New AI breakthrough",
+		details:
+			"Researchers at XYZ University have created a groundbreaking AI model that surpasses previous benchmarks in natural language understanding...",
+		image: "https://picsum.photos/400/200?random=1",
+	},
+];
+
 const NewsPage = () => {
-	return <div>News Page</div>;
+	return (
+		<div className="justify-self-center">
+			<div className="grid grid-cols-5 gap-6">
+				{newsData.map((news) => (
+					<NewsCard
+						key={news.id}
+						title={news.title}
+						details={news.details}
+						image={news.image}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
+
 export default NewsPage;
