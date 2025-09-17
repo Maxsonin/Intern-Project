6896ebc8fbadc4478e669081ae6daa1c5f0dfd7f
Maxsonin <maximles.lv@gmail.com>
2025-09-17 14:27:11 +0300

    hotfix: color fix

--- src/shared/components/layout/Navbar.tsx
+++ src/shared/components/layout/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 	];
 
 	return (
-		<header className="bg-blue-500 shadow-md py-4 px-6 flex justify-between items-center">
+		<header className="bg-blue-500 shadow-lg py-4 px-6 flex justify-between items-center">
 			<h1 className="text-2xl font-bold text-white">
 				<Link to="/">Intern News</Link>
 			</h1>
@@ -18,8 +18,8 @@ const Navbar = () => {
 						key={link.name}
 						to={link.path}
 						className={({ isActive }) =>
-							`font-medium text-gray-300 hover:text-white ${
-								isActive ? "text-blue-200 underline decoration-white" : ""
+							`font-medium hover:text-blue-300 ${
+								isActive ? "text-blue-900 underline" : "text-white"
 							}`
 						}
 					>
