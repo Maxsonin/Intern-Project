e037b5486439a0109aa08ab1ad527b11e3f3f633
Maxsonin <maximles.lv@gmail.com>
2025-10-04 19:04:15 +0300

    Task8(Sentry)

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+import Button from "@/shared/components/ui/Button";
+
 const HomePage = () => {
 	const adId = "ad-frame-0";
 
@@ -5,6 +7,15 @@ const HomePage = () => {
 		<>
 			<div className="text-xl font-bold">Welcome to №1 news website!</div>
 			<iframe id={adId} title="ad0" scrolling="no" frameBorder="0"></iframe>
+
+			<Button
+				className="mt-4 bg-pink-500 hover:bg-pink-700"
+				onClick={() => {
+					throw new Error("This is your first error!");
+				}}
+			>
+				Sentry Showcase
+			</Button>
 		</>
 	);
 };
